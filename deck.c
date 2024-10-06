215b254b3403d60c7f756f8197df5e1e515dd60b
Owen Raftery <oraftery@umass.edu>
2024-10-06 16:45:08 -0400

    shuffle method

--- deck.c
+++ deck.c
@@ -1,4 +1,5 @@
 #include "deck.h"
+#include "string.h"
 
 /*
  * Variable: deck_instance
@@ -7,3 +8,46 @@
  * Go Fish uses a single deck
  */
 struct deck deck_instance;
+
+/*
+ * Function: shuffle
+ * --------------------
+ *  Initializes deck_instance and shuffles it.
+ *  Resets the deck if a prior game has been played.
+ * 
+ *  returns: 0 if no error, and non-zero on error
+ */ 
+int shuffle() {
+    // initialize variables for deck creation
+    char suits[4] = {'S', 'H', 'D', 'C'};
+    char ranks[13] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'};
+    int deck_index = 0;
+
+    // fill deck with cards
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<13; j++) {
+            struct card to_add;
+            to_add.suit = suits[i];
+            strcpy(to_add.rank, ranks[j]); // strcpy needed here because the rank can he 1 OR 2 characters long
+
+            deck_instance.list[deck_index] = to_add;
+            deck_index++;
+        }
+    }
+
+    // initialize variables for deck creation
+    int random_index;
+    struct card temp_card;
+
+    // shuffle deck using Fisher-Yates shuffle (I googled this algo)
+    for(int i=0; i<=50; i++) {
+        random_index = (rand() % (51 - i + 1)) + i;
+        temp_card = deck_instance.list[i];
+        deck_instance.list[i] = deck_instance.list[random_index];
+        deck_instance.list[random_index] = temp_card;
+    }
+
+    deck_instance.top_card = 52; // ensure the last card is the top card
+
+    return 0;
+}
--- gofish.c
+++ gofish.c
@@ -3,4 +3,36 @@
 int main(int args, char* argv[]) 
 {
   fprintf(stdout, "Put your code here.");
+
+  /*
+    COMMENT PSUEDOCODE BEGINS
+
+    start program while loop
+    shuffle deck
+
+    start current game while loop
+    print hand and books
+    use a variable called round_num or something and check if it is even or odd. even means human picks card odd means robot picks
+    
+    EVEN:
+    prompt human player for rank
+    check valid rank
+    check robot hand for card and transfer cards to human player if robot possesses one, otherwise draw card
+    print this process
+    check for books in human, if they exist remove the 4 cards from hand and update books
+    check win condition and exit current while loop if true and declare winner
+
+    ODD:
+    randomly pick a rank in robot hand
+    check if human hand has card and transfer if needed, otherwise draw card
+    print this process
+    check for books in robot, if they exist remove the 4 cards from hand and update books
+    check win condition and exit current while loop if true and declare winner
+
+    exit game while loop
+    prompt user to want to play again
+    if yes then continue
+    if no then break from loop and exit program
+  */
+
 }
