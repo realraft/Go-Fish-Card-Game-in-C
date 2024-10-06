36e759911f3505de732b44020e85fbb8888a16ac
Owen Raftery <oraftery@umass.edu>
2024-10-06 13:59:34 -0400

    initial commit

---
+++ deck.c
@@ -0,0 +1,10 @@
+#include "deck.h"
+
+/*
+ * Variable: deck_instance
+ * -----------------------
+ *  
+ * Go Fish uses a single deck
+ */
+struct deck deck_instance;
+
---
+++ player.c
@@ -0,0 +1,11 @@
+#include "player.h"
+
+/*
+ * Instance Variables: user, computer   
+ * ----------------------------------
+ *  
+ *  We only support 2 users: a human and a computer
+ */
+struct player user;
+struct player computer;
+
